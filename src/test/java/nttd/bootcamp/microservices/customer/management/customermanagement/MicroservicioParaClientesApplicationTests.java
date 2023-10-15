cbd71f254011e5171ced7a6505de30984a40425d
jose mendoza sanchez <jmendozarv@outlook.com>
2023-10-15 09:15:05 -0500

    initial commit

---
+++ src/test/java/nttd/bootcamp/microservices/customer/management/customermanagement/MicroservicioParaClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package nttd.bootcamp.microservices.customer.management.customermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioParaClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
